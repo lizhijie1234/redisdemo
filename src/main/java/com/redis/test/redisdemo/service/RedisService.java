e96fdd666a7b504e4721045fe1f8d63cf19147b6
lizhijie <lijiazhijie@163.com>
2019-04-25 12:05:08 +0800

    redis demo

---
+++ src/main/java/com/redis/test/redisdemo/service/RedisService.java
@@ -0,0 +1,40 @@
+package com.redis.test.redisdemo.service;
+
+/**
+ * @description: todo
+ * @author: lizhijie
+ * @date: 19-4-25 上午11:11
+ */
+public interface RedisService {
+
+    /**
+     * set存数据
+     * @param key
+     * @param value
+     * @return
+     */
+    boolean set(String key, String value);
+
+    /**
+     * get获取数据
+     * @param key
+     * @return
+     */
+    String get(String key);
+
+    /**
+     * 设置有效天数
+     * @param key
+     * @param expire
+     * @return
+     */
+    boolean expire(String key, long expire);
+
+    /**
+     * 移除数据
+     * @param key
+     * @return
+     */
+    boolean remove(String key);
+
+}
